b4a70fc2d3c429610b8c2d258a06c61cac8e6455
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-02-07 16:34:16 +0000

    refactor: UCI helper module for encoding moves

---
+++ src/uci/mod.rs
@@ -0,0 +1,16 @@
+use crate::move_generator::move_data::{Flag, Move};
+
+pub fn encode_move(move_data: Move) -> String {
+    let mut encoded = String::with_capacity(4);
+    encoded.push_str(&move_data.from.to_notation());
+    encoded.push_str(&move_data.to.to_notation());
+
+    match move_data.flag {
+        Flag::QueenPromotion => encoded.push('q'),
+        Flag::RookPromotion => encoded.push('r'),
+        Flag::KnightPromotion => encoded.push('n'),
+        Flag::BishopPromotion => encoded.push('b'),
+        _ => {}
+    };
+    encoded
+}
