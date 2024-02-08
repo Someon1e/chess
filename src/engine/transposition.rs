7f622209c5964e20b36eaddcbf7d4164f6d80e6f
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-02-08 17:35:44 +0000

    refactor: Transposition capacity

--- src/engine/transposition.rs
+++ src/engine/transposition.rs
@@ -21,6 +21,6 @@ pub enum NodeType {
 pub const TRANSPOSITION_CAPACITY: usize = {
     const MEGABYTES: usize = 128;
 
-    const MEMORY_OF_ONE_ENTRY_IN_BYTES: usize = std::mem::size_of::<NodeValue>();
+    const MEMORY_OF_ONE_ENTRY_IN_BYTES: usize = std::mem::size_of::<Option<NodeValue>>();
     (MEGABYTES * 1000000) / MEMORY_OF_ONE_ENTRY_IN_BYTES
 };
