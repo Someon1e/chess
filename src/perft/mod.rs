8201956824ff60835d00c10684d387c2da23daab
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-02-09 20:36:36 +0000

    feat: UCI perft

---
+++ src/perft/mod.rs
@@ -0,0 +1,58 @@
+use crate::{
+    board::{zobrist::Zobrist, Board},
+    move_generator::MoveGenerator,
+    uci,
+};
+
+fn perft(board: &mut Board, check_zobrist: bool, bulk_count: bool, depth: u16) -> usize {
+    if depth == 0 {
+        return 1;
+    };
+
+    let mut move_count = 0;
+    MoveGenerator::new(board).gen(
+        &mut |move_data| {
+            if !bulk_count || depth != 1 {
+                board.make_move(&move_data);
+            }
+
+            if check_zobrist {
+                assert!(Zobrist::compute(board) == board.zobrist_key());
+            }
+
+            move_count += perft(board, check_zobrist, bulk_count, depth - 1);
+
+            if !bulk_count || depth != 1 {
+                board.unmake_move(&move_data);
+            }
+        },
+        false,
+    );
+
+    move_count
+}
+
+pub fn perft_root(board: &mut Board, check_zobrist: bool, bulk_count: bool, depth: u16) -> usize {
+    let mut move_count = 0;
+    MoveGenerator::new(board).gen(
+        &mut |move_data| {
+            if !bulk_count || depth != 1 {
+                board.make_move(&move_data);
+            }
+
+            if check_zobrist {
+                assert!(Zobrist::compute(board) == board.zobrist_key());
+            }
+
+            let inner = perft(board, check_zobrist, bulk_count, depth - 1);
+            println!("{}: {}", uci::encode_move(move_data), inner);
+            move_count += inner;
+
+            if !bulk_count || depth != 1 {
+                board.unmake_move(&move_data);
+            }
+        },
+        false,
+    );
+    move_count
+}
