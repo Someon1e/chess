8b08e63a32489d7051bab5dd0f1d797398eacfbb
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-02-16 12:25:32 +0000

    refactor: Crate for magic generation

--- engine/src/move_generator/slider_lookup.rs
+++ engine/src/move_generator/slider_lookup.rs
@@ -36,7 +36,7 @@ pub fn all_rays() -> &'static [[BitBoard; 8]; 65] {
     COMPUTATION.get_or_init(|| calculate_all_rays())
 }
 
-fn iterate_combinations(squares: BitBoard) -> impl core::iter::Iterator<Item = BitBoard> {
+pub fn iterate_combinations(squares: BitBoard) -> impl core::iter::Iterator<Item = BitBoard> {
     let mut next = Some(BitBoard::EMPTY);
     core::iter::from_fn(move || {
         let result = next;
@@ -48,7 +48,7 @@ fn iterate_combinations(squares: BitBoard) -> impl core::iter::Iterator<Item = B
     })
 }
 
-fn all_blockers(
+pub fn all_blockers(
     from: Square,
     directions: &[Direction],
     squares_from_edge: &[Direction],
@@ -63,7 +63,7 @@ fn all_blockers(
     bit_board
 }
 
-fn gen_rook_or_bishop(from: Square, blockers: &BitBoard, direction_offset: usize) -> BitBoard {
+pub fn gen_rook_or_bishop(from: Square, blockers: &BitBoard, direction_offset: usize) -> BitBoard {
     let mut moves = BitBoard::EMPTY;
 
     let rays = &all_rays()[from.usize()];
@@ -115,7 +115,7 @@ pub fn relevant_bishop_blockers() -> &'static [BitBoard; 64] {
     COMPUTATION.get_or_init(|| calculate_blockers_for_each_square(4, 8))
 }
 
-fn magic_index(blockers: &BitBoard, magic: u64, shift: u64) -> usize {
+pub fn magic_index(blockers: &BitBoard, magic: u64, shift: u64) -> usize {
     let hash = blockers.wrapping_mul(BitBoard::new(magic));
     (hash >> shift).as_usize()
 }
@@ -178,105 +178,11 @@ mod tests {
         move_generator::{
             precomputed::SQUARES_FROM_EDGE,
             slider_lookup::{
-                get_bishop_moves, get_rook_moves, relevant_bishop_blockers, relevant_rook_blockers,
-                Key,
+                all_blockers, gen_rook_or_bishop, get_bishop_moves, get_rook_moves,
+                iterate_combinations, relevant_bishop_blockers, relevant_rook_blockers,
             },
         },
     };
-
-    use super::{all_blockers, gen_rook_or_bishop, iterate_combinations, magic_index};
-
-    use rand_chacha::rand_core::{RngCore, SeedableRng};
-
-    #[derive(Debug)]
-    struct TableFillError;
-    fn fill_magic_table(
-        square: Square,
-        blockers: BitBoard,
-        magic: u64,
-        index_bits: u64,
-        direction_offset: usize,
-    ) -> Result<Vec<BitBoard>, TableFillError> {
-        let mut table = vec![BitBoard::EMPTY; 1 << index_bits];
-        for blocker_combination in iterate_combinations(blockers) {
-            let moves = gen_rook_or_bishop(square, &blocker_combination, direction_offset);
-            let table_entry = &mut table[magic_index(&blocker_combination, magic, 64 - index_bits)];
-            if table_entry.is_empty() {
-                *table_entry = moves;
-            } else if *table_entry != moves {
-                return Err(TableFillError);
-            }
-        }
-        Ok(table)
-    }
-
-    #[test]
-    fn find_rook_magics() {
-        let mut random = rand_chacha::ChaCha20Rng::seed_from_u64(1);
-
-        let mut length = 0;
-
-        let mut keys = [Key {
-            magic: 0,
-            shift: 0,
-            offset: 0,
-        }; 64];
-
-        for square_index in 0..64 {
-            let square = Square::from_index(square_index);
-
-            let blockers = relevant_rook_blockers()[square_index as usize];
-            let index_bits = blockers.count() as u64;
-            loop {
-                let magic = random.next_u64() & random.next_u64() & random.next_u64();
-                let filled = fill_magic_table(square, blockers, magic, index_bits, 0);
-                if let Ok(filled) = filled {
-                    keys[square_index as usize] = Key {
-                        magic: magic,
-                        shift: 64 - index_bits,
-                        offset: length,
-                    };
-
-                    length += filled.len();
-                    break;
-                }
-            }
-        }
-        println!("{keys:?} {length}");
-    }
-    #[test]
-    fn find_bishop_magics() {
-        let mut random = rand_chacha::ChaCha20Rng::seed_from_u64(1);
-
-        let mut length = 0;
-
-        let mut keys = [Key {
-            magic: 0,
-            shift: 0,
-            offset: 0,
-        }; 64];
-
-        for square_index in 0..64 {
-            let square = Square::from_index(square_index);
-            let blockers = relevant_bishop_blockers()[square_index as usize];
-            let index_bits = blockers.count() as u64;
-            loop {
-                let magic = random.next_u64() & random.next_u64() & random.next_u64();
-                let filled = fill_magic_table(square, blockers, magic, index_bits, 4);
-                if let Ok(filled) = filled {
-                    keys[square_index as usize] = Key {
-                        magic: magic,
-                        shift: 64 - index_bits,
-                        offset: length,
-                    };
-
-                    length += filled.len();
-                    break;
-                }
-            }
-        }
-        println!("{keys:?} {length}");
-    }
 
     #[test]
     fn blocker_combinations() {
---
+++ magic/src/main.rs
@@ -0,0 +1,106 @@
+use engine::{
+    board::{bit_board::BitBoard, square::Square},
+    move_generator::{
+        slider_keys::Key,
+        slider_lookup::{
+            gen_rook_or_bishop, iterate_combinations, magic_index, relevant_bishop_blockers,
+            relevant_rook_blockers,
+        },
+    },
+};
+
+use rand_chacha::rand_core::{RngCore, SeedableRng};
+
+#[derive(Debug)]
+struct TableFillError;
+fn fill_magic_table(
+    square: Square,
+    blockers: BitBoard,
+    magic: u64,
+    index_bits: u64,
+    direction_offset: usize,
+) -> Result<Vec<BitBoard>, TableFillError> {
+    let mut table = vec![BitBoard::EMPTY; 1 << index_bits];
+    for blocker_combination in iterate_combinations(blockers) {
+        let moves = gen_rook_or_bishop(square, &blocker_combination, direction_offset);
+        let table_entry = &mut table[magic_index(&blocker_combination, magic, 64 - index_bits)];
+        if table_entry.is_empty() {
+            *table_entry = moves;
+        } else if *table_entry != moves {
+            return Err(TableFillError);
+        }
+    }
+    Ok(table)
+}
+
+fn find_rook_magics() {
+    let mut random = rand_chacha::ChaCha20Rng::seed_from_u64(1);
+
+    let mut length = 0;
+
+    let mut keys = [Key {
+        magic: 0,
+        shift: 0,
+        offset: 0,
+    }; 64];
+
+    for square_index in 0..64 {
+        let square = Square::from_index(square_index);
+
+        let blockers = relevant_rook_blockers()[square_index as usize];
+        let index_bits = blockers.count() as u64;
+        loop {
+            let magic = random.next_u64() & random.next_u64() & random.next_u64();
+            let filled = fill_magic_table(square, blockers, magic, index_bits, 0);
+            if let Ok(filled) = filled {
+                keys[square_index as usize] = Key {
+                    magic: magic,
+                    shift: 64 - index_bits,
+                    offset: length,
+                };
+
+                length += filled.len();
+                break;
+            }
+        }
+    }
+    println!("{keys:?} {length}");
+}
+
+fn find_bishop_magics() {
+    let mut random = rand_chacha::ChaCha20Rng::seed_from_u64(1);
+
+    let mut length = 0;
+
+    let mut keys = [Key {
+        magic: 0,
+        shift: 0,
+        offset: 0,
+    }; 64];
+
+    for square_index in 0..64 {
+        let square = Square::from_index(square_index);
+        let blockers = relevant_bishop_blockers()[square_index as usize];
+        let index_bits = blockers.count() as u64;
+        loop {
+            let magic = random.next_u64() & random.next_u64() & random.next_u64();
+            let filled = fill_magic_table(square, blockers, magic, index_bits, 4);
+            if let Ok(filled) = filled {
+                keys[square_index as usize] = Key {
+                    magic: magic,
+                    shift: 64 - index_bits,
+                    offset: length,
+                };
+
+                length += filled.len();
+                break;
+            }
+        }
+    }
+    println!("{keys:?} {length}");
+}
+
+fn main() {
+    find_rook_magics();
+    find_bishop_magics();
+}
