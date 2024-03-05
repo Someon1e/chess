f87dba64b7000d19082086c40c2676e1d8a6ad4d
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-03-05 17:25:05 +0000

    feat: Improve null move pruning

--- engine/src/search/mod.rs
+++ engine/src/search/mod.rs
@@ -209,7 +209,7 @@ impl<'a> Search<'a> {
             self.board.make_null_move();
 
             let score = -self.negamax(
-                ply_remaining - 3,
+                ply_remaining - (3 + ply_remaining / 6),
                 ply_from_root + 1,
                 false,
                 should_cancel,
@@ -219,7 +219,7 @@ impl<'a> Search<'a> {
             self.board.unmake_null_move();
 
             if score >= beta {
-                return beta;
+                return score;
             }
         }
 
