ee329bf168a16b4fad6b1e99558e11930faf796b
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-04-13 17:30:01 +0100

    docs: Document move_data

--- engine/src/move_generator/move_data.rs
+++ engine/src/move_generator/move_data.rs
@@ -3,21 +3,33 @@ use core::fmt;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum Flag {
+    /// A standard move
     None,
 
+    /// A pawn promoting to a queen
     QueenPromotion,
+
+    /// A pawn promoting to a rook
     RookPromotion,
+
+    /// A pawn promoting to a bishop
     BishopPromotion,
+
+    /// A pawn promoting to a knight
     KnightPromotion,
 
+    /// En passant
     EnPassant,
 
+    /// A pawn moving two squares up
     PawnTwoUp,
 
+    /// The king castling
     Castle,
 }
 
 impl Flag {
+    /// Every flag
     pub const ALL: [Self; 8] = [
         Self::None,
         Self::QueenPromotion,
@@ -28,6 +40,8 @@ impl Flag {
         Self::PawnTwoUp,
         Self::Castle,
     ];
+
+    /// Every promotion flag
     pub const PROMOTIONS: [Self; 4] = [
         Self::QueenPromotion,
         Self::RookPromotion,
@@ -35,6 +49,17 @@ impl Flag {
         Self::KnightPromotion,
     ];
 
+    /// Returns the piece being promoted into if it is a promotion flag, otherwise returns None
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use engine::{move_generator::move_data::Flag, board::piece::Piece};
+    ///
+    /// assert_eq!(Flag::QueenPromotion.get_promotion_piece(true), Some(Piece::WhiteQueen));
+    /// assert_eq!(Flag::BishopPromotion.get_promotion_piece(false), Some(Piece::BlackBishop));
+    /// assert_eq!(Flag::Castle.get_promotion_piece(true), None);
+    /// ```
     #[must_use]
     pub const fn get_promotion_piece(&self, white: bool) -> Option<Piece> {
         match self {
@@ -71,10 +96,16 @@ impl Flag {
     }
 }
 
+/// A piece move
 #[derive(PartialEq, Eq, Clone, Copy)]
 pub struct Move {
+    /// Square the piece is moving from
     pub from: Square,
+
+    /// Square the piece is moving to
     pub to: Square,
+
+    /// Type of move
     pub flag: Flag,
 }
 
