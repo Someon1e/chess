41e081b35224590155a081febae1cc05ed254f61
Someon1e <142684596+Someon1e@users.noreply.github.com>
2024-06-16 15:26:23 +0100

    bench: Create board outside of evaluation

--- engine/benches/evaluation.rs
+++ engine/benches/evaluation.rs
@@ -5,27 +5,30 @@ use engine::{
 };
 
 pub fn evaluation_benchmark(c: &mut Criterion) {
+    let start_position = Board::from_fen(
+        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+    );
     c.bench_function("evaluate start position", |bencher| {
         bencher.iter(|| {
-            let board = Board::from_fen(black_box(Board::START_POSITION_FEN));
             Eval::evaluate(
                 &eval_data::MIDDLE_GAME_PIECE_SQUARE_TABLES,
                 &eval_data::END_GAME_PIECE_SQUARE_TABLES,
                 &eval_data::PHASES,
-                &board,
+                black_box(&start_position),
             )
         });
     });
+
+    let kiwipete = Board::from_fen(
+        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+    );
     c.bench_function("evaluate kiwipete", |bencher| {
         bencher.iter(|| {
-            let board = Board::from_fen(black_box(
-                "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
-            ));
             Eval::evaluate(
                 &eval_data::MIDDLE_GAME_PIECE_SQUARE_TABLES,
                 &eval_data::END_GAME_PIECE_SQUARE_TABLES,
                 &eval_data::PHASES,
-                &board,
+                black_box(&kiwipete),
             )
         });
     });
